b9bb0ca306881432070d059c616cf8a3dbd48bbb
Luis_Felipe_Chiqueto <lfchiqueto@gmail.com>
2025-04-14 13:46:40 +0000

    feat: finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,33 @@
+//Firebase
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+//Material UI
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//Páginas internas
+import 'package:pokedex/firebase_options.dart';
+import 'package:pokedex/pages/login_page.dart';
+import 'package:pokedex/pages/register_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCiChfG9-FdZLj7Snixe18xr54_jWKi2LI",
+          authDomain: "pokedex-app-50a65.firebaseapp.com",
+          projectId: "pokedex-app-50a65",
+          storageBucket: "pokedex-app-50a65.firebasestorage.app",
+          messagingSenderId: "536470323024",
+          appId: "1:536470323024:web:f84d5400f55e89d5c2f655"));
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
